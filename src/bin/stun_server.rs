e9bda467ede2845ed70c22183918e4bbeff5d60a
hickey <596310898@163.com>
2024-10-28 15:15:32 +0800

    construct a simple strn server

--- src/bin/stun_server.rs
+++ src/bin/stun_server.rs
@@ -1,3 +1,31 @@
-fn main(){
-    println!("Hello, world!");
+use tokio::net::UdpSocket;
+use stun::message::*;
+use stun::server::*;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "0.0.0.0:3478".parse()?;
+    let socket = UdpSocket::bind(addr).await?;
+    println!("TURN server listening on {}", addr);
+
+    let mut buf = [0; 1024];
+
+    loop {
+        let (len, src) = socket.recv_from(&mut buf).await?;
+        println!("Received packet from {}", src);
+
+        let mut msg = Message::new();
+        msg.raw = buf[..len].to_vec();
+
+        if let Err(e) = msg.decode() {
+            eprintln!("Failed to decode STUN message: {}", e);
+            continue;
+        }
+
+        let mut res = Message::new();
+        res.build(&msg)?;
+
+        let res_buf = res.raw;
+        socket.send_to(&res_buf, &src).await?;
+    }
 }
